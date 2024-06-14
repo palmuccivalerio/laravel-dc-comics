9484013b8b5bdaaa9c061ce2c0cdc6236e12fa81
palmuccivalerio <palmuccivalerio@gmail.com>
2024-06-14 17:21:46 +0200

    add:seeder

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comic;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,18 @@ class ComicsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+          $data = config('comics');
+          foreach($data as $item){
+            $newComic = new Comic();
+            $newComic->title = $item['title'];
+            $newComic->description = $item['description'];    
+            $newComic->thumb = $item['thumb'];    
+            $newComic->price = $item['price'];
+            $newComic->series = $item['series'];
+            $newComic->sale_date = $item['sale_date'];
+            $newComic->type = $item['type']; 
+            $newComic->save();
+          
     }
+}
 }
