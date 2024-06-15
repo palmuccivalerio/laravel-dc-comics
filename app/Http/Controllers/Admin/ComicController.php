a44dcaf46accd9534441816b113b5c352fc8c1d7
palmuccivalerio <palmuccivalerio@gmail.com>
2024-06-15 12:21:03 +0200

    add:show

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -36,9 +36,9 @@ class ComicController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Comic $comic)
     {
-        //
+        return view('layouts.comics.show',compact('comic'));
     }
 
     /**
