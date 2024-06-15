fcad1758434876008b534c1705ee80f1a4f777ad
palmuccivalerio <palmuccivalerio@gmail.com>
2024-06-15 12:31:57 +0200

    add:showdetails

--- resources/views/layouts/comics/index.blade.php
+++ resources/views/layouts/comics/index.blade.php
@@ -11,7 +11,7 @@
                 <th scope="col">id</th>
                 <th scope="col">titolo</th>
                 <th scope="col">tipologia</th>
-                <th scope="col">actions</th>
+                <th scope="col">azioni</th>
             </tr>
         </thead>
         <tbody>
@@ -20,7 +20,7 @@
                 <th scope="row">{{ $item->id }}</th>
                 <td>{{ $item->title }}</td>
                 <td>{{ $item->type }}</td>
-                <td></td>
+                <td><a href="{{route('comics.show',['comic'=>$item->id])}}">Dettagli</a></td>
             </tr>    
             @endforeach
          
